583b829335df3bfff316b3457162a09d4acbe43a
elionaifigueiredo <elionaifigueiredo@gail.com>
2021-07-22 14:34:22 -0400

    tela de authenticação

--- core/views.py
+++ core/views.py
@@ -2,14 +2,40 @@ from django.http import response
 from core.models import Evento
 from django.shortcuts import redirect, render, HttpResponse
 from core.models import Evento
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     return redirect('/agenda/')
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error (request, 'Usuario ou senha não confere')
+    return redirect ('/')
+    
+
+
+@login_required(login_url='/login/')
 def lista_evento(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'a': evento}
     return render(request, 'agenda.html', dados)
