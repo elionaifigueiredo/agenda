e71accabf10fbb19d21f26eabc5e8da87ee22dd5
elionaifigueiredo <elionaifigueiredo@gail.com>
2021-07-23 10:51:19 -0400

    feito atualização e exclusao de dados da agenda

--- core/models.py
+++ core/models.py
@@ -7,7 +7,8 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
-    data_criacao = models.DateTimeField(verbose_name='Data da Criação', auto_now=True) 
+    data_criacao = models.DateTimeField(verbose_name='Data da Criação', auto_now=True)
+    local = models.CharField(blank=True, null=True, max_length=100)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -15,4 +16,7 @@ class Evento(models.Model):
     
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%MHrs')
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
